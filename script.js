c6c22e5c0ef94e5099d6678c380453c650fe52a3
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-04-09 16:25:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
-const key = Object.keys(sampleObject).filter(item => items === true);
+let key = Object.keys(sampleObject).filter(item => items === true);
 }
 
 Do not change the code below

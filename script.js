6ca29bd1947777ace4e55b738e410892b44e30a6
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-04-13 18:13:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
-var key=sampleObject.hasOwnProperty('key');
-return key;
+function hasKey(key) {
+	for (let index of sampleObject) {
+		if(index === key)return true
+		
+	}
+	return false;
 }
+
+
 
 Do not change the code below
 const key = prompt("Enter Key.");

d86e542f5c1509927e6cde5fd9dd27d885f43911
Aniket Kule <38484786+Aniket230297@users.noreply.github.com>
2023-04-14 10:47:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
-function hasKey(key) {
-	for (let i in sampleObject) {
-		if(i === key){ return true;}else{return false;}
-		
-	}
-	
-}
 
-Do not change the code below
+	   if(Object.keys(sampleObject) === key){
+		   return true;
+	   }else{
+		   return false;
+		   
+	   }
+
+//Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key));
 
